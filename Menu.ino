2904f788f7ac7c06739ceb9700e14b0a8f4afd1a
j272zhou <j272zhou@06e43772-ae1b-4c0d-9892-c0dabbf93334>
2016-11-24 17:02:26 +0000

    added character and menu

---
+++ Menu.ino
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <string.h>
+
+static enum GamePages
+{Welcome = 0;
+Feeding = 1;
+Cleaning = 2;
+Medicine = 3;
+Stats = 4;
+MinigameSelect = 5;
+Bopit = 6;
+Speed = 7;
+Simon = 8;
+  } gameUiPage = Welcome;
