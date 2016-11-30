563bd2c8020705814e77cef0b39f477264449c10
a66lam <a66lam@06e43772-ae1b-4c0d-9892-c0dabbf93334>
2016-11-30 00:04:43 +0000

    Modified character functions and added more bitmaps.

--- Home.ino
+++ Home.ino
@@ -1,14 +1,16 @@
 static void homeScreen(){
-   printTama(frog);
+   printTama(frog, revFrog);
    homeBackground();
    potentiometer();
    OrbitOledUpdate();
    delay(70);
+   OrbitOledClearBuffer();
 }
 
 static void homeBackground(){
   OrbitOledMoveTo(64, 0);
   OrbitOledLineTo(64, 32);
-  OrbitOledMoveTo(68, 0);
+
+
 }
 
